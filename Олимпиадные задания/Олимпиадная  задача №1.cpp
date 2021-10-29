02ffc2ad7b93b1f6348f770adc759b25c2ac8f6b
Матаков Михаил РИС-21-1Б <92533785+MihailMatakov@users.noreply.github.com>
2021-10-30 01:05:29 +0500

    Add files via upload

---
+++ Олимпиадные задания/Олимпиадная  задача №1.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL,"Russian");
+	int x;
+	cout << "Введите число:";
+	cin >> x;
+	while ((x < 100) || (x > 999))
+	{
+		cout << "Число введено не верно. Введите число заного: \n";
+		cin >> x;
+	}
+	int a, b, c;
+	a = x % 10;
+	x = x / 10;
+	b = x % 10;
+	c = x / 10;
+	while ((a == b) || (b == c) || (a == c))
+	{
+		cout << "Цифры в числе не могут быть одинаковыми. Введите число заного:";
+		cin >> x;
+		a = x % 10;
+		x = x / 10;
+		b = x % 10;
+		c = x / 10;
+	}
+	cout << "Все возможные числа \n";
+	cout << a << b << c << "\n";
+	cout << a << c << b << "\n";
+	cout << b << a << c << "\n";
+	cout << b << c << a << "\n";
+	cout << c << b << a << "\n";
+	cout << c << a << b << "\n";
+	cout << "Наибольшее число из всех:";
+	if ((a > b) && (a > c))
+	{
+		if (b > c)
+		{
+			cout << a << b << c;
+		}
+		else
+		{
+			cout << a << c << b;
+		}
+
+	}
+	else
+	{
+		if ((b > c) && (b > a))
+		{
+			if (a > c)
+			{
+				cout << b << a << c;
+			}
+			else
+			{
+				cout << b << c << a;
+			}
+		}
+		else
+		{
+			if (a > b)
+			{
+				cout << c << a << b;
+			}
+			else
+			{
+				cout << c << b << c;
+			}
+
+	     }
+
+	}
+	return 0;
+}
---
+++ Олимпиадные задания/Олимпиадная задача №2.cpp
@@ -0,0 +1,45 @@
+﻿#include<iostream>;
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "");
+	int a;
+	int	k, g; 
+	int i;
+	k = 0;
+	g = 0;
+	srand(time(NULL));
+	a = rand() % 100 + 1;
+	cout << ("Угадай число из диапазано от 1 до 100 за 7 попыток.\n");
+	while ((k < 7) && (g < 1))
+	{
+		cout << ("введите число:\n");
+		cin >> i;
+		if (i > a)
+		{
+			cout << "Число должно быть меньше\n"; 
+			k = k + 1;
+		}
+		else
+		{
+			if (i < a)
+			{
+				cout << "Число должно быть меньше больше\n"; 
+				k = k + 1;
+			}
+			else
+			{
+				if (i = a)
+				{
+					cout << "Число введено верно: " << a; 
+					g = g + 1;
+				}
+			}
+		}
+		if (k == 7)
+		{
+			cout << "Попытки закончились. Загаданное чсло:" << a;
+		}
+	}
+	return 0;
+}
