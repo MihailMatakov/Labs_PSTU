9a69cbdc3a51ee68b7c9098f90722692a821d4c7
Матаков Михаил РИС-21-1Б <92533785+MihailMatakov@users.noreply.github.com>
2021-11-04 00:04:28 +0500

    Update 4.1.3 Константа присвоение декремент.cpp

--- Задание №4/4.1(по структуре)/4.1.3 Константа присвоение декремент.cpp
+++ Задание №4/4.1(по структуре)/4.1.3 Константа присвоение декремент.cpp
@@ -3,10 +3,14 @@
 using namespace std;
 int main() 
 {
+	// Константа
 	const int a = 10;
+	// Переменная  целочисленного типа
 	int b, c, d;
 	b = 20;
+	//Присваивание
 	d = b;
+	// Декремент
 	c = --d;
 	cout << a << endl;
 	cout << b << endl;
