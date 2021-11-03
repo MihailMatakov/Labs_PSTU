30f7a9f616fff510bb7d6d9618353c8a6ee3ea6e
Матаков Михаил РИС-21-1Б <92533785+MihailMatakov@users.noreply.github.com>
2021-11-03 23:37:12 +0500

    Add files via upload

---
+++ Задание №4/4.1(по структуре)/4.1.3 Константа присвоение декремент.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+int main() 
+{
+	const int a = 10;
+	int b, c, d;
+	b = 20;
+	d = b;
+	c = --d;
+	cout << a << endl;
+	cout << b << endl;
+	cout << c;
+	return 0;
+}
---
+++ Задание №4/4.3(ввод-вывод)/4.3.1 Умножение.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a, s, d, f, g;
+    cout << "Введите символы \n";
+    сin >> a >> s >> d >> f >> g;
+    cout << "Результат \n";
+    cin >> g >> f >> d >> s >> a;
+	return 0;
+}
