258969284003ade082ff22d406e02f8d23bd5927
Матаков Михаил РИС-21-1Б <92533785+MihailMatakov@users.noreply.github.com>
2021-11-04 00:02:15 +0500

    Update 4.1.2 Галон фут.cpp

--- Задание №4/4.1(по структуре)/4.1.2 Галон фут.cpp
+++ Задание №4/4.1(по структуре)/4.1.2 Галон фут.cpp
@@ -1,14 +1,17 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 int main()
 {
+	// Локализация программы
 	setlocale(LC_ALL, "");
+	// Переменная с плавающей точкой
 	float f, g;
 	cout << "Введите количество галлонов\n";
 	cin >> g;
+	// Чтобы найти кубические фунты надо галонны разделить на 7.481
 	f = g / 7.481;
 	cout << "Количество кубических футов:\n";
 	cout << f;
 	return 0;
 }
+
